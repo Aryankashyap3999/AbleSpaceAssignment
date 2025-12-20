bf63e9e9b114d0cbefd747ff96babc6abff9c850
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-12-20 17:32:10 +0530

    fix cors error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,10 +34,7 @@ declare global {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true // Allow cookies to be sent with requests
-}));
+app.use(cors());
 app.use(cookieParser());
 
 // Attach io to request object for controllers
